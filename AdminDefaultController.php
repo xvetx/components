32f5c26b729da05227f610fbc8fe273ce2d92c3a
vi mark <webvimark@gmail.com>
2014-10-28 09:28:14 +0100

    renderisajax moved to the basecontroller

--- AdminDefaultController.php
+++ AdminDefaultController.php
@@ -251,26 +251,6 @@ class AdminDefaultController extends BaseController
 	}
 
 	/**
-	 * Render ajax or usual depends on request
-	 *
-	 * @param string $view
-	 * @param array $params
-	 *
-	 * @return string|\yii\web\Response
-	 */
-	protected function renderIsAjax($view, $params)
-	{
-		if ( Yii::$app->request->isAjax )
-		{
-			return $this->renderAjax($view, $params);
-		}
-		else
-		{
-			return $this->render($view, $params);
-		}
-	}
-
-	/**
 	 * Finds the model based on its primary key value.
 	 * If the model is not found, a 404 HTTP exception will be thrown.
 	 *
--- BaseController.php
+++ BaseController.php
@@ -6,4 +6,23 @@ use Yii;
 
 class BaseController extends AccessController
 {
+	/**
+	 * Render ajax or usual depends on request
+	 *
+	 * @param string $view
+	 * @param array $params
+	 *
+	 * @return string|\yii\web\Response
+	 */
+	protected function renderIsAjax($view, $params = [])
+	{
+		if ( Yii::$app->request->isAjax )
+		{
+			return $this->renderAjax($view, $params);
+		}
+		else
+		{
+			return $this->render($view, $params);
+		}
+	}
 }
